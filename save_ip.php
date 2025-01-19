83a87064060a9fcf7a0c92b49b08f9f15bf26f1c
LRR502 <157323646+LRR502@users.noreply.github.com>
2025-01-19 20:56:06 +0100

    Create save_ip.php

---
+++ save_ip.php
@@ -0,0 +1,10 @@
+<?php
+// filepath: save_ip.php
+$data = json_decode(file_get_contents('php://input'), true);
+$ip = $data['ip'];
+
+$file = 'ips.txt';
+file_put_contents($file, $ip . PHP_EOL, FILE_APPEND);
+
+echo 'IP saved successfully';
+?>
