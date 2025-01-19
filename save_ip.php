5db0bbdc66f1c2f8650fce8d638ce4d95c2e5c6c
LRR502 <157323646+LRR502@users.noreply.github.com>
2025-01-19 20:57:31 +0100

    Update save_ip.php

--- save_ip.php
+++ save_ip.php
@@ -3,7 +3,7 @@
 $data = json_decode(file_get_contents('php://input'), true);
 $ip = $data['ip'];
 
-$file = 'ips.txt';
+$file = 'ip.txt';
 file_put_contents($file, $ip . PHP_EOL, FILE_APPEND);
 
 echo 'IP saved successfully';
